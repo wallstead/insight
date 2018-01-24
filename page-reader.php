860809cb598ef4efffa6e3a64c98a6ee6f35196c
Willis Allstead <willallstead@icloud.com>
2018-01-24 08:45:53 -0800

    adding embeded posts

---
+++ page-reader.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package BF_FUTURETASTIC
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+            <div class="page">
+				<h1 class="p1">Reader</h1>
+
+            </div>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+// get_sidebar();
+get_footer();
